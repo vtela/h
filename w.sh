8fac3de79844bb5d9a461748831a2b930ce0110f
vtela <53336233+vtela@users.noreply.github.com>
2021-05-11 15:36:45 +0800

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#curl -ssL https://raw.fastgit.org/vtela/h/main/w.sh -O
-#source ./w.sh
+#curl -ssL https://raw.fastgit.org/vtela/h/main/w.sh -o /tmp/w.sh
+#source /tmp/w.sh
 /usr/bin/mv -f $HOME/.bash_history{,.bak}
 unset HISTORY HISTFILE HISTSAVE HISTZONE HISTORY HISTLOG
 export HISTFILE=/dev/null
